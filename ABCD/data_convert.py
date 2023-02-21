50032022e299c31d479d967211e03a31204e0733
Arshitha <arsh2794@gmail.com>
2023-02-21 18:58:17 -0500

    adding stdout message for data dicts without corresponding data files

--- ABCD/data_convert.py
+++ ABCD/data_convert.py
@@ -16,9 +16,14 @@ def help_text():
     return args
 
 
-def data_summary(data_with_no_dict):
-    print(f"A total of {len(data_with_no_dict)} data files don't have a corresponding data dictionary.")
+def data_summary(data_with_no_dict, dict_no_data_files):
+    print(
+        f"A total of {len(data_with_no_dict)} data files in the ABCD Release 4.0 tabular data package don't have a corresponding data dictionary.")
     print(data_with_no_dict)
+
+    print(
+        f"A total of {len(dict_no_data_files)} data dictionaries don't have a corresponding data file in the ABCD release 4.0 tabular data package.")
+    print(dict_no_data_files)
 
 
 def main():
@@ -31,10 +36,17 @@ def main():
     outdir = Path('phenotype')
 
     # extract nda short names into a list
+    data_files = list(tab_data_dir.glob('*.txt'))
     data_short_names = [i.name.split('.txt')[0] for i in
-                        tab_data_dir.glob('*.txt')]  # assert that this is indeed a list of strings.
+                        data_files]  # assert that this is indeed a list of strings.
     data_dicts = list(data_dict_dir.glob('*.csv'))
     data_files_no_dict = []
+    dict_no_data_files = []
+    for dict in data_dicts:
+        dict_prefix = dict.name.split('.')[0]
+        expected_data_file = tab_data_dir.joinpath(dict_prefix + '.csv')
+        if expected_data_file not in data_files:
+            dict_no_data_files.append(expected_data_file.name)
 
     for d in data_short_names:
         expected_dict = data_dict_dir.joinpath(d + '.csv')  # constructing dictionary filename
@@ -54,7 +66,7 @@ def main():
         with open(outdir.joinpath(d + '.tsv'), 'w') as new_f:
             wr = csv.writer(new_f, delimiter='\t', quoting=csv.QUOTE_MINIMAL)
             wr.writerows(rows)
-    data_summary(data_files_no_dict)
+    data_summary(data_files_no_dict, dict_no_data_files)
 
 
 if __name__ == '__main__':
